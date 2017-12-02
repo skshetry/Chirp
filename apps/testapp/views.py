91e7420adc57e89275e96c1741a4f5fb2d4aa5d3
skshetry <suagatchhetri@outlook.com>
2017-12-02 19:42:57 +0545

    Added error for trying sentry.

--- apps/testapp/views.py
+++ apps/testapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
+from . import logger
 import os
 
 # Create your views here.
 def test_view(request):
     send_mail('Subject here','Here is the message.','from@example.com',[os.environ.get('DJANGO_ADMINS_EMAIL_ADDRESS',default='acb@pahcjsdjsdkskssdskdskdjskdjsdsdskdjskdksjkdksexample.com')],fail_silently=False,)
     #email failing?
+    logger.error("Error occured.")
     return render(request, 'a.html')
