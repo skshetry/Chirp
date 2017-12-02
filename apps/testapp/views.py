b062e86b23b106eb4f39765742266589a2491467
skshetry <suagatchhetri@outlook.com>
2017-12-02 17:22:26 +0545

    Added email support.

--- apps/testapp/views.py
+++ apps/testapp/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-
+from django.conf import settings
+import os
 
 # Create your views here.
 def test_view(request):
-    # send_mail(
-    #     'Subject here',
-    #     'Here is the message.',
-    #     'from@example.com',
-    #     ['to@example.com'],
-    #     fail_silently=False,
-    # )
+    send_mail(
+        'Subject here',
+        'Here is the message.',
+        'from@example.com',
+        [os.environ.get('DJANGO_ADMINS_EMAIL_ADDRESS',default='acb@pahcjsdjsdkskssdskdskdjskdjsdsdskdjskdksjkdksexample.com')],
+        fail_silently=False,
+    )
     return render(request, 'a.html')
