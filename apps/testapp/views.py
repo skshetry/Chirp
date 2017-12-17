c5930d06aee884090bfe2560dcd0d750e07512d5
Divyamani Adhikari <dma.htd@gmail.com>
2017-12-17 23:24:16 +0545

    Added 404 and 500 error pages

--- apps/testapp/views.py
+++ apps/testapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
+from django.http import Http404
 import logging
 import os
 from .models import Images, ImageUploadForm
@@ -12,7 +13,10 @@ def test_view(request):
     send_mail('Subject here','Here is the message.','from@example.com',[os.environ.get('DJANGO_ADMINS_EMAIL_ADDRESS',default='acb@pahcjsdjsdkskssdskdskdjskdjsdsdskdjskdksjkdksexample.com')],fail_silently=False,)
     #email failing?
     logger.error("Error occured.")
-    return render(request, 'a.html')
+    try:
+        return render(request, 'a.html')
+    except request.DoesNotExist:
+        raise Http404('Page not found')
 
 def upload_view(request):
     all_images = all_images = Images.objects.all()
