f752119ce7e82cdafa1d34b345673d928ab86fb3
Divyamani Adhikari <dma.htd@gmail.com>
2017-12-21 21:05:45 +0545

    Added the error app, error handlers and tests

--- apps/errors/views.py
+++ apps/errors/views.py
@@ -1,13 +1,23 @@
+"""This contains views for the possible errors that could occur."""
+
 from django.shortcuts import render
 
+
 def server_error(request):
-    return render(request, 'errors/500.html')
+    """View for Internal Server Errors."""
+    return render(request, '500.html', status=500)
+
 
 def not_found(request):
-    return render(request, 'errors/404.html')
+    """View if no page/resource is found."""
+    return render(request, '404.html', status=404)
+
 
 def permission_denied(request):
-    return render(request, 'errors/403.html')
+    """View in case user requests for inaccesible item."""
+    return render(request, '403.html', status=403)
+
 
 def bad_request(request):
-    return render(request, 'errors/400.html')
+    """View for bad requests."""
+    return render(request, '400.html', status=400)
