e27f89c9590c2c60ec410f63d0bbe3f205bcbdd5
skshetry <suagatchhetri@outlook.com>
2017-12-23 17:29:29 +0545

    Add tests for csrf token in login form.

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,17 @@
+"""Tests for accounts app."""
 from django.test import TestCase
-
+from django.shortcuts import reverse
 # Create your tests here.
+
+
+class LoginTest(TestCase):
+    """This tests the login view."""
+
+    def setUp(self):
+        """Set things up for testing login functionality."""
+        self.url = reverse('accounts:login')
+        self.response = self.client.get(self.url)
+
+    def test_csrf(self):
+        """Test for csrf token."""
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
