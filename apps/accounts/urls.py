e821266efe2e3cb3cbe41908ad291deabeee80c7
skshetry <suagatchhetri@outlook.com>
2017-12-25 09:38:05 +0545

    Changed the view to remove clutter from **homegrown** view to **LoginView**.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-
-from .views import login_view, logout_view, signup
+from .views import UserLoginView, logout_view, signup
 
 
 urlpatterns = [
     url(r'signup/$', signup, name='signup'),
-    url(r'^login/$', login_view, name='login'),
+    url(r'^login/$', UserLoginView.as_view(), name='login'),
     url(r'^logout/$', logout_view, name='logout'),
-
 ]
