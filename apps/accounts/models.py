15a97e35b452d416ba4856fb6aba0bbff77b5770
Paurakh Sharma Humagain <paurakh011@gmail.com>
2017-12-26 21:12:07 +0545

    After creating signup

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.models import User
 # Create your models here.
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User)
+    
