24f332623ccb09ec6cbdb45d37ca239c8fb95938
skshetry <suagatchhetri@outlook.com>
2017-12-26 08:06:20 +0545

    Adds documentation.

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
+"""Form handling for :accounts:."""
+from django import forms
 
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -17,6 +19,12 @@ class SignUpForm(UserCreationForm):
 
 
 class CustomUserAuthenticationForm(AuthenticationForm):
+    """
+    CustomUserAuthForm with custom error message.
+
+    Also, adds attributes to the input fields.
+    """
+
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -27,8 +35,8 @@ class CustomUserAuthenticationForm(AuthenticationForm):
         )
     )
     password = forms.CharField(
-        widget = forms.PasswordInput(
-            attrs = {
+        widget=forms.PasswordInput(
+            attrs={
                 'class': 'form-control',
                 'placeholder': 'Password',
                 'required': 'required',
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,13 +1,15 @@
+"""Views for :accounts: handling."""
 from django.shortcuts import render, redirect
 
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import CustomUserAuthenticationForm as UserAuthenticationForm
 from .forms import SignUpForm
 
 
 def signup(request):
+    """Create user."""
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -23,10 +25,21 @@ def signup(request):
 
 
 class UserLoginView(LoginView):
+    """
+    This handles login view.
+
+    This logs in user using `UserAuthenticationForm`
+    and redirects if user is already logged in.
+    If there's `next` parameter, it redirects to it after :POST:.
+    If login fails, the same view is shown with helpful error message.
+    """
+
     template_name = 'accounts/login.html'
     authentication_form = UserAuthenticationForm
     redirect_authenticated_user = True
 
+
 def logout_view(request):
+    """Logout the user."""
     logout(request)
     return redirect('accounts:login')
