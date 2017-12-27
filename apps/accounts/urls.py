a7f7487dd92b5c883aba270de1845e8f451e79d8
skshetry <suagatchhetri@outlook.com>
2017-12-27 22:58:48 +0545

    Fixed login not working.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from . import views
+from .views import UserLoginView, logout_view, signup, activate as activate_view
 
-urlpatterns=[
-    url(r'signup/$', views.signup, name='signup'),
+
+urlpatterns = [
+    url(r'signup/$', signup, name='signup'),
+    url(r'^login/$', UserLoginView.as_view(), name='login'),
+    url(r'^logout/$', logout_view, name='logout'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+        activate_view, name='activate'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
+"""Views for :accounts: handling."""
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
-from django.contrib.sites.shortcuts import get_current_site
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from django.http import HttpResponse
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
-from .tokens import account_activation_token
-from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
+
+from .forms import CustomUserAuthenticationForm as UserAuthenticationForm
+from .forms import SignUpForm
+from .tokens import account_activation_token
 
 
 def signup(request):
@@ -20,8 +25,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('Home')
+            return redirect('accounts:login')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
@@ -41,3 +45,24 @@ def activate(request, uidb64, token):
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
         return HttpResponse('Activation link is invalid!')
+
+
+class UserLoginView(LoginView):
+    """
+    This handles login view.
+
+    This logs in user using `UserAuthenticationForm`
+    and redirects if user is already logged in.
+    If there's `next` parameter, it redirects to it after :POST:.
+    If login fails, the same view is shown with helpful error message.
+    """
+
+    template_name = 'accounts/login.html'
+    authentication_form = UserAuthenticationForm
+    redirect_authenticated_user = True
+
+
+def logout_view(request):
+    """Logout the user."""
+    logout(request)
+    return redirect('accounts:login')
