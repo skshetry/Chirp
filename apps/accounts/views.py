fecc72106e21b78f57b2d3c5e5a25b8e9f2abdaf
skshetry <suagatchhetri@outlook.com>
2017-12-27 17:33:43 +0545

    Fixed merge.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -18,7 +18,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('Home')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
