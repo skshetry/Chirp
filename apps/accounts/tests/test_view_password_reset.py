f906b4fcc50e0bd4ca32b0fac1ef92c5bedf3d33
hotrod <yathomasi1@gmail.com>
2017-12-30 23:13:38 +0545

    adds PasswordResetTests

---
+++ apps/accounts/tests/test_view_password_reset.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.core import mail
+from django.core.urlresolvers import reverse
+from django.urls import resolve
+from django.test import TestCase
+
+class PasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, PasswordResetForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain two inputs: csrf and email
+        '''
+        self.assertContains(self.response, '<input', 2)
+        self.assertContains(self.response, 'type="email"', 1)
