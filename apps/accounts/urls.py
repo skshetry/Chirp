88647b33605a4fb2c0ffaf9bfd75d137677622f0
hotrod <yathomasi1@gmail.com>
2017-12-31 09:29:29 +0545

    indentation and redirect link

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -22,7 +22,10 @@ urlpatterns = [
         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
         name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html',
+            success_url = reverse_lazy('accounts:password_reset_complete'),
+        ),
         name='password_reset_confirm'),
     url(r'^reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
