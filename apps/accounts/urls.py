c987663e814738cf37e8bf564968089bfb73dfbc
hotrod <yathomasi1@gmail.com>
2017-12-31 08:24:58 +0545

    adds reverse_lazy for success redirect

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from .views import UserLoginView, logout_view, signup, activate as activate_view
 from django.contrib.auth import views as auth_views
-
+from django.core.urlresolvers import reverse_lazy
+app_name = 'accounts'
 urlpatterns = [
     url(r'signup/$', signup, name='signup'),
     url(r'^login/$', UserLoginView.as_view(), name='login'),
@@ -13,7 +14,8 @@ urlpatterns = [
         auth_views.PasswordResetView.as_view(
             template_name='password_reset.html',
             email_template_name='password_reset_email.html',
-            subject_template_name='password_reset_subject.txt'
+            subject_template_name='password_reset_subject.txt',
+            success_url = reverse_lazy('accounts:password_reset_done'),
         ),
         name='password_reset'),
     url(r'^reset/done/$',
