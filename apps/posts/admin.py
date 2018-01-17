c0cabb52b20141fffd0434e805b49025601464a4
skshetry <suagatchhetri@outlook.com>
2018-01-17 18:48:17 +0545

    Add admin support for posts

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, PostMedia, PostsMetadata, Tag
+
+
+class PostMediaAdminInline(admin.StackedInline):
+    model = PostMedia
+
+
+class PostsMetadataAdminInline(admin.StackedInline):
+    model = PostsMetadata
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostMediaAdminInline, PostsMetadataAdminInline]
+
+
+admin.site.register(Post, admin_class=PostAdmin)
+admin.site.register(Tag)
+admin.site.register(PostsMetadata)
