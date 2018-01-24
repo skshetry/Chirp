5ce58e689c102d2caedc607f07f064ee58be4702
divyamani <dma.htd@gmail.com>
2018-01-24 22:48:22 +0545

    added views for user profile and profile picture changing

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Photo
+from .forms import PhotoForm
 
 
 @login_required
 def user_profile(request):
     """The User Profile page view."""
     return render(request, 'user_profile.html')
+
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('settings:settings')
+    else:
+        form = PhotoForm()
+    return render(request, 'image_list.html', {'rform': form, 'photos': photos})
