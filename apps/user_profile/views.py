6c219f4f5349e5f1eab447ef452ed903ace39c07
divyamani <dma.htd@gmail.com>
2018-01-26 19:52:58 +0545

    Fixed the user form not saving issue

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Photo
-from .forms import PhotoForm
+# from .models import Photo
+# from .forms import PhotoForm
 
 
 @login_required
@@ -10,13 +10,13 @@ def user_profile(request):
     return render(request, 'user_profile.html')
 
 
-def photo_list(request):
-    photos = Photo.objects.all()
-    if request.method == 'POST':
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('settings:settings')
-    else:
-        form = PhotoForm()
-    return render(request, 'image_list.html', {'rform': form, 'photos': photos})
+# def photo_list(request):
+#     photos = Photo.objects.all()
+#     if request.method == 'POST':
+#         form = PhotoForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('settings:settings')
+#     else:
+#         form = PhotoForm()
+#     return render(request, 'image_list.html', {'rform': form, 'photos': photos})
