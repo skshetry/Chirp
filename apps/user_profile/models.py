6f2952795258af5bf89958f6836e4954ba30b3ff
divyamani <dma.htd@gmail.com>
2018-01-26 19:50:33 +0545

    Fixed issues with the user details models

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class User_details(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_details')
     date_of_birth = models.DateField(null=True)
     bio = models.TextField(max_length=300, null=True)
     GENDER_CHOICES = (
@@ -17,20 +17,15 @@ class User_details(models.Model):
         choices=GENDER_CHOICES,
         null=True,
     )
+    # file = models.ImageField()
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         User_details.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-
-class Photo(models.Model):
-    file = models.ImageField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name = 'photo'
-        verbose_name_plural = 'photos'
+    instance.user_details.save()
