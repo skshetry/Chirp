3079310abea288318a01769d329ae0400ae97456
divyamani <dma.htd@gmail.com>
2018-01-26 19:49:53 +0545

    Modified the user detais forms

--- apps/user_profile/forms.py
+++ apps/user_profile/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.files import File
 # from testapp.models import Images
 from django.contrib.auth.models import User
-from .models import Photo, User_details
+from .models import User_details
 
 
 class UserForm(forms.ModelForm):
@@ -21,34 +21,34 @@ class UserDetailsForm(forms.ModelForm):
         max_length=2,
         widget=forms.Select(choices=User_details.GENDER_CHOICES),
     )
-    date_of_birth = forms.DateField()
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'class': 'datepicker form-control'}))
+    # profile_photo = forms.ImageField()
 
     class Meta:
         model = User_details
         fields = ('bio', 'gender', 'date_of_birth',)
-        widgets = {'date_of_birth': forms.DateInput(attrs={'id': 'datetimepicker12'})}
 
-class PhotoForm(forms.ModelForm):
-    x = forms.FloatField(widget=forms.HiddenInput())
-    y = forms.FloatField(widget=forms.HiddenInput())
-    width = forms.FloatField(widget=forms.HiddenInput())
-    height = forms.FloatField(widget=forms.HiddenInput())
+# class PhotoForm(forms.ModelForm):
+#     x = forms.FloatField(widget=forms.HiddenInput())
+#     y = forms.FloatField(widget=forms.HiddenInput())
+#     width = forms.FloatField(widget=forms.HiddenInput())
+#     height = forms.FloatField(widget=forms.HiddenInput())
 
-    class Meta:
-        model = Photo
-        fields = ('file', 'x', 'y', 'width', 'height', )
+#     class Meta:
+#         model = Photo
+#         fields = ('file', 'x', 'y', 'width', 'height', )
 
-    def save(self):
-        photo = super(PhotoForm, self).save()
+#     def save(self):
+#         photo = super(PhotoForm, self).save()
 
-        x = self.cleaned_data.get('x')
-        y = self.cleaned_data.get('y')
-        w = self.cleaned_data.get('width')
-        h = self.cleaned_data.get('height')
+#         x = self.cleaned_data.get('x')
+#         y = self.cleaned_data.get('y')
+#         w = self.cleaned_data.get('width')
+#         h = self.cleaned_data.get('height')
 
-        image = photo.open(Photo.file)
-        cropped_image = photo.crop((x, y, w+x, h+y))
-        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
-        resized_image.save(photo.file.path)
+#         image = photo.open(Photo.file)
+#         cropped_image = photo.crop((x, y, w+x, h+y))
+#         resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+#         resized_image.save(photo.file.path)
 
-        return photo
+#         return photo
