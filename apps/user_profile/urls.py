2df160aadd729b40b1a0be52de8aef068d5f7784
divyamani <dma.htd@gmail.com>
2018-01-26 19:51:29 +0545

    Removed the change photo link

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.shortcuts import reverse
 from django.urls import reverse_lazy
-from .views import user_profile, photo_list
+from .views import user_profile # , photo_list
 
 
 urlpatterns = [
     url(r'^$', user_profile, name='user_profile'),
-    url(r'^photo$', photo_list, name='photo_list'),
+    # url(r'^photo$', photo_list, name='photo_list'),
     ]
