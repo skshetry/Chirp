e75a669cc3c22a7c291e17557a9c0c87edab07a7
divyamani <dma.htd@gmail.com>
2018-02-01 00:39:38 +0545

    Change User Profile view

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,8 +1,8 @@
+from PIL import Image
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-# from .models import Photo
-# from .forms import PhotoForm
+from .models import User_details
 
 
 @login_required
@@ -14,15 +14,5 @@ def user_profile(request, username=None):
             "user": user,
         })
     else:
-        return render("User not found")
-
-# def photo_list(request):
-#     photos = Photo.objects.all()
-#     if request.method == 'POST':
-#         form = PhotoForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('settings:settings')
-#     else:
-#         form = PhotoForm()
-#     return render(request, 'image_list.html', {'rform': form, 'photos': photos})
+        return ("User not found")
+    
