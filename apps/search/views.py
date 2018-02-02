8ff94359c7353e9047d4556249a99cc7e6d2dced
yathomasi <yathomasi@gmail.com>
2018-02-02 13:18:47 +0545

    added imports for search views

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import json
 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from apps.posts.models import Post
+from apps.decorators import ajax_required
 # Create your views here.
