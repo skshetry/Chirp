4f50fbbee2d594729956e30d02639e8cafb556d2
yathomasi <yathomasi@gmail.com>
2018-02-02 20:42:52 +0545

    Added Search view

--- apps/search/views.py
+++ apps/search/views.py
@@ -9,3 +9,36 @@ from django.contrib.auth.decorators import login_required
 from apps.posts.models import Post
 from apps.decorators import ajax_required
 # Create your views here.
+
+def search(request):
+    if 'q' in request.GET:
+        querystring = request.GET.get('q').strip()
+        if len(querystring) == 0:
+            return redirect('/search/')
+        try:
+            search_type = request.GET.get('type')
+            if search_type not in ['posts', 'users']:
+                search_type = 'posts'
+        except Exception:
+            search_type = 'posts'
+        
+        count = {}
+        results = {}
+        results['posts'] = Post.objects.filter(text__icontains=querystring, parent=None)
+        results['users'] = User.objects.filter(
+            Q(username__icontains=querystring) | Q(
+                first_name__icontains=querystring) | Q(
+                    last_name__icontains=querystring))
+        count['posts'] = results['posts'].count()
+        count['users'] = results['users'].count()
+
+        return render(request, 'search/results.html', {
+            'hide_search': True,
+            'querystring': querystring,
+            'active': search_type,
+            'count': count,
+            'results': results[search_type],        
+        })
+    else:
+        return render(request, 'search/search.html', {'hide_search': True})
+
