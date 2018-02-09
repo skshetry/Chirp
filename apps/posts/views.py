7682a5489015a14d3cfb8adb86f7f2a1a6570f0b
Saugat Pachhai <suagatchhetri@outlook.com>
2018-02-09 18:34:19 +0545

    Add view to add post
    The view has a post form and a formset that generates form for media file uploads and takes care of all the uploads
    and validation.

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from .forms import PostForm, PostMediaFormSet
 
-# Create your views here.
+
+def posts_add(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        media_form_set = PostMediaFormSet(request.POST, request.FILES)
+        if post_form.is_valid() and media_form_set.is_valid():
+            post = post_form.save(request.user)
+            media_form_set.save(post)
+            return redirect(reverse('Home'))
+    elif request.method == 'GET':
+        post_form = PostForm()
+        media_form_set = PostMediaFormSet()
+
+    return render(request, 'posts/post_add.html', {
+            'mediaformset': media_form_set,
+            'post_form': post_form,
+            })
