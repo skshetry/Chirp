bfe9a495beffcac5ca1cd6472976073322557464
yathomasi <yathomasi@gmail.com>
2018-02-13 22:16:46 +0545

    commented since no use

--- apps/search/static/js/search.js
+++ apps/search/static/js/search.js
@@ -1,6 +1,7 @@
-$(function () {
-    $(".posts-results li").click(function () {
-      var posts = $(this).attr("post-id");
-      location.href = "/posts/" + posts + "/"; //TODO change link to posts
-    });
-});
+// $(function () {
+//     $(".posts-results li").click(function () {
+//       var posts = $(this).attr("post-id");
+//       location.href = "/posts/" + posts + "/"; //TODO change link to posts
+//     });
+// });
+//May contradict uncomment if necessary later 
