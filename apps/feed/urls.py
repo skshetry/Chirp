afb4138a54bd4e0624a7c32836a2cc16c1fb2133
Saugat Pachhai <suagatchhetri@outlook.com>
2018-02-14 19:51:10 +0545

    Realtime feed

--- apps/feed/urls.py
+++ apps/feed/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import HomeView
+from .views import HomeView, retrieve_new_post
 
 urlpatterns=[
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^new-feed/(?P<post_id>[0-9a-f-]+)', retrieve_new_post, name="new_posts_retrieve_ajax"),
 ]
--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from django.core import serializers
+from django.http import JsonResponse
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from posts.forms import PostMediaFormSet, PostForm
+from posts.models import Post
+from django.db.models import F 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'feed/home.html'
@@ -13,3 +20,17 @@ class HomeView(LoginRequiredMixin, TemplateView):
         context['post_form'] = PostForm()
         return context
 
+
+@login_required
+def retrieve_new_post(request, post_id):
+    created_on = Post.objects.filter(
+        pk=post_id
+        ).values_list(
+            'created',
+            flat=True
+            )
+    new_feed = Post.objects.filter(
+        feed__user=request.user
+        ).order_by('-created').filter(created__gt=created_on).annotate(postuser=F('user__username'))[:100]
+    serialized_response = serializers.serialize('json', new_feed)
+    return JsonResponse(serialized_response, safe=False)
