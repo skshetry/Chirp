684e062ff1836e720963c1832dca3e9bfac56018
Saugat Pachhai <suagatchhetri@outlook.com>
2018-03-02 21:17:39 +0545

    Add api to feed

--- apps/feed/urls.py
+++ apps/feed/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 
-from .views import HomeView, retrieve_new_post
+from .views import HomeView, retrieve_new_post, get_feed_lists, get_recent_after, get_posts_before
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^new-feed/(?P<post_id>[0-9a-f-]+)',
         retrieve_new_post, name="new_posts_retrieve_ajax"),
+    url(r'^get-feed-list/', get_feed_lists, name='get_feed_list_ajax'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^get-recent-after/(?P<post_id>[0-9a-f-]+)', get_recent_after, name='get_recent_posts_ajax'),
+    url(r'^get-posts-before/(?P<post_id>[0-9a-f-]+)', get_posts_before, name='get_posts_before_ajax'),
+
 ]
--- apps/feed/views.py
+++ apps/feed/views.py
@@ -10,6 +10,9 @@ from django.views.generic import TemplateView
 from posts.forms import PostForm, PostMediaFormSet
 from posts.models import Post
 
+from .models import Feed
+from .serializers import FeedSerializer
+
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'feed/home.html'
@@ -42,3 +45,47 @@ def retrieve_new_post(request, post_id):
 
     serialized_response = json.dumps(list(new_feed), cls=DjangoJSONEncoder)
     return JsonResponse(serialized_response, safe=False)
+
+
+@login_required
+def get_feed_lists(request):
+    feed = Feed.objects.filter(user=request.user).order_by('-updated_on')[:100]
+    print(feed)
+    serialized_data = FeedSerializer(feed, context={'request': request}, many=True).data
+    return JsonResponse(serialized_data, safe=False)
+
+
+def get_recent_after(request, post_id):
+    created_on = Feed.objects.filter(
+        post_id=post_id,
+        user=request.user,
+    ).values_list(
+        'updated_on',
+        flat=True
+    )
+    new_feed = Feed.objects.filter(
+        user=request.user
+        ).filter(
+            updated_on__gt=created_on[0]
+            ).order_by('-updated_on')[:100]
+
+    serialized_data = FeedSerializer(new_feed, context={'request': request}, many=True).data
+    return JsonResponse(serialized_data, safe=False)
+
+
+def get_posts_before(request, post_id):
+    created_on = Feed.objects.filter(
+        post_id=post_id,
+        user=request.user,
+    ).values_list(
+        'updated_on',
+        flat=True
+    )
+    new_feed = Feed.objects.filter(
+        user=request.user
+        ).filter(
+            updated_on__lt=created_on[0]
+            ).order_by('-updated_on')[:100]
+
+    serialized_data = FeedSerializer(new_feed, context={'request': request}, many=True).data
+    return JsonResponse(serialized_data, safe=False)
